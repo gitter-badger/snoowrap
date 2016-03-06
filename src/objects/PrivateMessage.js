76f14ac8fd80db739123dba01a7137927c65f22b
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-03-06 17:24:48 -0500

    Incomplete framework for toolbox usernotes
    [ci skip]

--- src/objects/PrivateMessage.js
+++ src/objects/PrivateMessage.js
@@ -7,7 +7,7 @@ const helpers = require('../helpers');
 */
 const PrivateMessage = class extends require('./ReplyableContent') {
   get _uri () {
-    return `message/messages/${this.name.slice(3)}`;
+    return `message/messages/${this.id}`;
   }
   _transform_api_response (response_obj) {
     return helpers.find_message_in_tree(this.name, response_obj[0]);
